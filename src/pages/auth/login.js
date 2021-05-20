55f44289701a9d0fb2c934933575be175e5684b9
Muhammad Ibrar <muhammadibrar1@yahoo.com>
2021-05-20 12:29:37 +0500

    bug fix

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -49,18 +49,7 @@ export default function Example() {
               </div>
   
               <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                 
-                  <label htmlFor="remember_me" className="ml-2 block text-sm text-gray-900">
-                    Remember me
-                    <input
-                    id="remember_me"
-                    name="remember_me"
-                    type="checkbox"
-                    className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-                  />
-                  </label>
-                </div>
+              
   
                 <div className="text-sm">
                   <a href="#w" className="font-medium text-indigo-600 hover:text-indigo-500">
